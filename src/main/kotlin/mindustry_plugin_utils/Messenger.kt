92eb4480871a2c7832fd925a8097bbfab122c6fc
Jakub Dóka <mlokogegelmlok@gmail.com>
2021-05-11 15:19:18 +0200

    adding text template

--- src/main/kotlin/mindustry_plugin_utils/Messenger.kt
+++ src/main/kotlin/mindustry_plugin_utils/Messenger.kt
@@ -1,9 +1,13 @@
 package mindustry_plugin_utils
 
+import com.beust.klaxon.Klaxon
+import java.io.File
+import java.lang.Exception
+
 class Messenger(
     private val prefix: String = "Messenger",
     private val verboseMessage: String = "verbose is disabled",
-    private val verbose: Boolean
+    private val verbose: Boolean = false
 ) {
     fun verbose(r: () -> Unit) {
         if(verbose) r.invoke()
---
+++ src/main/kotlin/mindustry_plugin_utils/TextTemplates.kt
@@ -0,0 +1,7 @@
+package mindustry_plugin_utils
+
+object Templates {
+    fun info(title: String, body: String, color: String = "orange"): String {
+        return "[$color]$title[]\n\n$body"
+    }
+}
